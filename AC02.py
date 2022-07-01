de3feb92a591566cfc2af5d0bed4388c530e5b2a
MarcosSoFi <marcos.soutofilgueira@gmail.com>
2022-07-01 18:41:12 +0100

    Upload 2 - unittest - Added

--- AC02.py
+++ AC02.py
@@ -60,3 +60,34 @@ def test_total1():
 def test_total2():
 	assert total([1,2,3]) > 5
 
+
+#   [2] unittest
+#	----------------------------------------------------------------
+class Tests(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual(string_ToInt1("1"),1)
+
+	def test_2(self):
+		self.assertIsInstance(string_ToInt1("1"),int)
+
+	def test_3(self):
+		self.assertIsInstance(string_ToInt1(True),int)
+
+	def test_4(self):
+		self.assertIsInstance(string_ToInt2("6"),int)
+
+	def test_5(self):
+		self.assertIsInstance(string_ToInt3("'1'")[0],int)
+
+	def test_6(self):
+		self.assertIsInstance(string_ToInt3("a")[0],int)
+
+	def test_7(self):
+		self.assertEqual(total([1,2,3]),6)
+
+	def test_8(self):
+		self.assertGreater(total([1,2,3]),5)
+
+
+if __name__ == "__main__":
+     unittest.main()
